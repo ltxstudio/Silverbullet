695f00dd02d96f2bda03d3117d3aa8066b343986
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-24 12:44:15 +0600

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <footer className="bg-gray-900 text-white py-6">
+    <div className="container mx-auto text-center">
+      <p>© 2024 Silverbullet. All rights reserved.</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
