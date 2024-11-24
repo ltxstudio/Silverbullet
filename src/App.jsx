ed5d6462c34462a34f4a202299ff06126fd0b4f2
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-24 13:00:31 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
-import Features from './pages/Features';
-import NotFound from './pages/NotFound';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
@@ -16,14 +13,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/features" element={<Features />} />
-            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
         <Footer />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
