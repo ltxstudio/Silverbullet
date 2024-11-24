a2479ab47abc585bfd36925cbb01a3c14719a3ea
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-24 12:43:23 +0600

    Create DownloadButton.jsx

---
+++ src/components/DownloadButton.jsx
@@ -0,0 +1,10 @@
+const DownloadButton = () => (
+  <a
+    href="#"
+    className="inline-block bg-blue-500 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-600"
+  >
+    Download Now
+  </a>
+);
+
+export default DownloadButton;
